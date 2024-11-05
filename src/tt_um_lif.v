c69f54896cd5a0974834590da3b22a8d62bf47cc
katrogacheva <katherinerogacheva@gmail.com>
2024-11-05 12:14:54 -0800

    Bypass tests 2.0

--- src/tt_um_lif.v
+++ src/tt_um_lif.v
@@ -20,5 +20,6 @@ module tt_um_lif (
 
   // instantiate lif neuron
   lif lif1 (.current(ui_in), .clk(clk), .reset_n(rst_n), .state(uo_out), .spike(uio_out[7]));
+  //lif lif2 (.current({{uio_out[7], 7'b0000000}}), .clk(clk), .reset_n(rst_n), .state(uo_out), .spike(uio_out[7]));
 
 endmodule
