d561945f7aa7b369abf743d4126914823498aa70
katrogacheva <katherinerogacheva@gmail.com>
2024-11-05 21:48:12 -0800

    Renamed all files for tinytapeout submission

--- src/tt_um_delta_liafn.v
+++ src/tt_um_delta_liafn.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module delta_liafn (
+module tt_um_delta_liafn (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
