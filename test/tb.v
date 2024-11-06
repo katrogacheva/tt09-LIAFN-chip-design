f29f81c674eb8bcee4a79f180543924a6201a542
katrogacheva <katherinerogacheva@gmail.com>
2024-11-05 21:42:44 -0800

    Tinytapeout submission please

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  delta_liafn user_project (
+  tt_um_delta_liafn user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
